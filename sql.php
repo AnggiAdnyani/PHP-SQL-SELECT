8090f2a993bf0abf82cdf0bcaac99db3fda19954
agatha_fysco <90663406+AnggiAdnyani@users.noreply.github.com>
2023-11-20 10:28:41 +0800

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "localhost";
+    $user   = "root";
+    $password = "";
+    $database = "2101020031_dewaayuanggiadnyani";
+
+    $con    = mysqli_connect($server, $user, $password, $database);
+?>
